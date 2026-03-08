fac61dc2503e9a4f3e9181a538d67088cb4c7fc4
John Angelakis <aggioannis@gmail.com>
2026-03-08 15:19:36 +0200

    fix port for deploy

--- my-web-app/server/src/index.js
+++ my-web-app/server/src/index.js
@@ -208,8 +208,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(publicDir, "index.html"));
 });
 
-app.listen(3001, () => {
-  console.log("Server running on http://localhost:3001");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 // GET /api/history?userId=1
