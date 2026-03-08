54bed6c5f352cd898b673f55533a5eba12f6477d
John Angelakis <aggioannis@gmail.com>
2026-03-08 16:50:55 +0200

    Import fix

--- my-web-app/server/scripts/importPatients.js
+++ my-web-app/server/scripts/importPatients.js
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 import { db } from "../src/db.js";
 
-const XRAY_DIR = path.join(process.cwd(), "../../../Final Xray Collection");
+const XRAY_DIR = path.join(process.cwd(), "../Final Xray Collection");
 
 async function run() {
 
